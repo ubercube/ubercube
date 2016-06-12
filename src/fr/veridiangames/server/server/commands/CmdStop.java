4bdfec504e0b0b7fbf9eb13f5190f18927c8388e
Tybau <gomusic.sisium@gmail.com>
2016-06-12 12:44:55 +0200

    Ubercube Pre-Alpha 1 - Copyright correction (Tybau)

--- src/fr/veridiangames/server/server/commands/CmdStop.java
+++ src/fr/veridiangames/server/server/commands/CmdStop.java
@@ -1,20 +1,20 @@
 /*
  * Copyright (C) 2016 Team Ubercube
  *
- *   This file is part of Ubercube.
+ * This file is part of Ubercube.
  *
- *       Ubercube is free software: you can redistribute it and/or modify
- *       it under the terms of the GNU General Public License as published by
- *       the Free Software Foundation, either version 3 of the License, or
- *       (at your option) any later version.
+ *     Ubercube is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
  *
- *       Ubercube is distributed in the hope that it will be useful,
- *       but WITHOUT ANY WARRANTY; without even the implied warranty of
- *       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *       GNU General Public License for more details.
+ *     Ubercube is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
  *
- *       You should have received a copy of the GNU General Public License
- *       along with Ubercube.  If not, see http://www.gnu.org/licenses/.
+ *     You should have received a copy of the GNU General Public License
+ *     along with Ubercube.  If not, see http://www.gnu.org/licenses/.
  */
 
 package fr.veridiangames.server.server.commands;
